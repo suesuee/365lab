9be158d02f8f2ec982dbe01a36f6e214ef3528f7
Sue Sue <29suesue@gmail.com>
2024-09-30 14:32:00 -0700

    update because I changed my root folder name

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/suesue/365lab/.venv"
+set -gx VIRTUAL_ENV "/Users/suesue/potion365/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
